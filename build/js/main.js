cd8ca42f9ae6901f66c29cf54f1aff84a9a24aaa
MansAhmadBONgit config --global user.name MansAhmadBONgit config --global user.name MansAhmadBON <mansue.ahmad.94@mail.ru>
2019-11-20 12:41:41 +0300

    feat: added all components sectionCase

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,56 @@
 window.onload = function(){
+    const caseSliderCtrlsBottom = document.querySelector('.caseSliderCtrls-bottom');
+
+    function deletCurrentIcoActive(){
+        const currentIcoActive = document.querySelector('.icoActive');
+        const currentItemActive = document.querySelector('.itemActive');
+        currentIcoActive.classList.remove('icoActive');
+        currentItemActive.classList.remove('itemActive')
+    }
+
+    caseSliderCtrlsBottom.addEventListener('click', function(e){
+        const elem = e.target.className.replace(/ctrlMob__ico ctrlMob__ico-/gi,'');
+        //let barCtrls = document.querySelector('')
+        let icoActive = null;
+        let activItem = null;
+        switch(elem){
+            case 'mob':
+                activItem = document.querySelector('.caseSliderItems__item-3')
+                icoActive = document.querySelector('.icoCtrlActive-mob');
+                deletCurrentIcoActive()
+                activItem.classList.add('itemActive');
+                icoActive.classList.add('icoActive');
+                caseSliderCtrlsBottom.classList.add('barPosition-1')
+                break;
+            case 'tabl':
+                activItem = document.querySelector('.caseSliderItems__item-2')
+                icoActive = document.querySelector('.icoCtrlActive-tabl');
+                deletCurrentIcoActive()
+                activItem.classList.add('itemActive')
+                icoActive.classList.add('icoActive');
+                caseSliderCtrlsBottom.classList.remove('barPosition-1')
+                break;
+            case 'desc':
+                activItem = document.querySelector('.caseSliderItems__item-1')
+                icoActive = document.querySelector('.icoCtrlActive-desc');
+                deletCurrentIcoActive()
+                activItem.classList.add('itemActive')
+                icoActive.classList.add('icoActive');
+                caseSliderCtrlsBottom.classList.remove('barPosition-1')
+                break;
+            default: return null;
+        }
+    })
+    
+
+
+
+
+
+
+
+
+    //video slider
     $('.slidersBox').slick({
         autoplay: true,
         autoplaySpeed: 2000,
